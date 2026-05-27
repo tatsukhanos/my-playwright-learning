f5ab1679ebe1d6e42b5256f5132c3106986c6c5e
Tatsiana <tatsiana.sukhanos@solvd.com>
2026-05-26 17:25:28 -0700

    Add login regression tests with LoginPage

---
+++ test-data/users.ts
@@ -0,0 +1,11 @@
+export const users = {
+  standard: {
+    username: 'standard_user',
+    password: 'secret_sauce',
+  },
+
+  lockedOut: {
+    username: 'locked_out_user',
+    password: 'secret_sauce',
+  },
+};
---
+++ tests/login.spec.ts
@@ -0,0 +1,68 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { users } from '../test-data/users';
+
+
+test.describe('SauceDemo tests', () => {
+  test.beforeEach(async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.open();
+  });
+
+  test('Test 1 - Valid user can log in and see inventory page', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+    await loginPage.login(
+  users.standard.username,
+  users.standard.password
+);
+
+    await expect(
+      page,
+      'User should be redirected to inventory page after successful login'
+    ).toHaveURL(/inventory/);
+  });
+
+  test('Test 2 - Locked out user cannot log in and sees error message', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+
+   await loginPage.login(
+  users.lockedOut.username,
+  users.standard.password
+);
+
+    await expect(
+      loginPage.errorMessage,
+      'Locked out user should see lockout error message'
+    ).toContainText('Epic sadface: Sorry, this user has been locked out.');
+  });
+
+test('Test 3 - Wrong password shows error message', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.login(
+    users.standard.username,
+    'wrong_password'
+  );
+
+  await expect(
+    loginPage.errorMessage,
+    'User should see error message when password is wrong'
+  ).toContainText('Username and password do not match');
+});
+
+test('Test 4 - Empty username shows validation error', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  await loginPage.login(
+    '',
+    users.standard.password
+  );
+
+  await expect(
+    loginPage.errorMessage,
+    'User should see validation error when username is empty'
+  ).toContainText('Username is required');
+});
+
+});
